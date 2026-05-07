5d41256f581f8ff5009caf832e67a46b7c0a3671
Soha0089 <mahmudx0010@gmail.com>
2026-05-07 14:51:42 +0600

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,14 +1,16 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    const target = "https://mahmud-global-apihub-cgxr.onrender.com";  
-    const targetUrl = target + url.pathname + url.search;  
 
-    const response = await fetch(targetUrl, {  
-      method: request.method,  
-      headers: request.headers,  
-      body: request.method === "GET" ? null : request.body  
-    });  
+    const target = "https://mahmud-global-apihub-cgxr.onrender.com";
+
+    const targetUrl = target + url.pathname + url.search;
+
+    const response = await fetch(targetUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.method === "GET" || request.method === "HEAD" ? null : request.body,
+    });
 
     return response;
   }
